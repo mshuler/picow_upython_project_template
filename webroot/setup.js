81f5f3cb88a13d54db8549798e8f0fd83a6f65ae
Michael Shuler <michael@pbandjelly.org>
2022-09-10 14:45:05 -0500

    Fix HTML whitespace and move style.css

--- webroot/setup.js
+++ webroot/setup.js
@@ -10,55 +10,55 @@ let networkList = []
 let selectedWiFiNetworkIndex = 0;
 
 var log = function(msg) {
-   $('<span/>').html(msg + '\n').appendTo('#result')
+  $('<span/>').html(msg + '\n').appendTo('#result')
 };
-	  	
+
 showPassCB.addEventListener("click", function() {
-    if (wifiPasswordField.type === "password") {
-        wifiPasswordField.type = "text";
-    } else {
-        wifiPasswordField.type = "password";
-    }
-});   
+  if (wifiPasswordField.type === "password") {
+    wifiPasswordField.type = "text";
+  } else {
+    wifiPasswordField.type = "password";
+  }
+});
 
 function updateSelectedNetwork(){
-    return function(){
-	     let network = networkList[wifiNetworksField.selectedIndex];
-	     channelField.value = network[2];
-	     rssiField.value = network[3];
-	     wifiSSIDField.value = network[0];
-    }
+  return function(){
+    let network = networkList[wifiNetworksField.selectedIndex];
+    channelField.value = network[2];
+    rssiField.value = network[3];
+    wifiSSIDField.value = network[0];
+  }
 }
 
 wifiNetworksField.addEventListener("click", function() {
-	setTimeout(updateSelectedNetwork(), 2000);
+  setTimeout(updateSelectedNetwork(), 2000);
 });
 
 function ssidOK() {
-	 if( wifiSSIDField.value.length < 8 ) {
-	 	alert("The SSID must be at least 8 characters long.");
-	 	return 0;
-	 }
-	 else {
-    	return 1;
-	 }
+  if( wifiSSIDField.value.length < 8 ) {
+    alert("The SSID must be at least 8 characters long.");
+    return 0;
+  }
+  else {
+    return 1;
+  }
 }
 
 function passwordOK() {
-	// Password can be left unset for open networks
- 	return 1;
+  // Password can be left unset for open networks
+  return 1;
 }
 
 function saveWiFiSettings() {
-    console.log("PJA: SAVE WIFI SETTINGS");
+  console.log("PJA: SAVE WIFI SETTINGS");
 }
 
 window.onload = function(){
-   let nStrings = scannedWifiNetworksField.value.split(",");
-   for( index in nStrings) {
-   	elems = nStrings[index].split(":");
-   	networkList.push(elems);
-   	wifiNetworksField.add(new Option(elems[0]));
-   	console.log( elems );
-   }
+  let nStrings = scannedWifiNetworksField.value.split(",");
+  for( index in nStrings) {
+    elems = nStrings[index].split(":");
+    networkList.push(elems);
+    wifiNetworksField.add(new Option(elems[0]));
+    console.log( elems );
+  }
 }
